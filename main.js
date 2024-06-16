e617b9b1bfe9b3215bdcd399cdbefd150210bd96
AbdulRehman1770 <rehman71gtx@gmail.com>
2024-06-16 18:02:42 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+let ToDo_List = [];
+let condition = true;
+while (condition) {
+    let Activity = await inquirer.prompt({
+        name: "Task",
+        message: "Enter You Task",
+        type: "input"
+    });
+    ToDo_List.push(Activity.Task);
+    console.log(`${Activity.Task} Task SuccessFully Added In Your List1`);
+    let confirm = await inquirer.prompt({
+        name: "cfrm",
+        type: "confirm",
+        message: "Do You Want To Add More Task",
+        default: "False"
+    });
+    condition = confirm.cfrm;
+}
+console.log(`Your Task in To Do List is ${ToDo_List.join(" , ")}`);
